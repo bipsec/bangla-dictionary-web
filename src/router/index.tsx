e00daf36363f59a827e118d6d1916f4f79684a2c
bipsec <bip.sec22@gmail.com>
2023-09-02 01:20:44 +0600

    feat: fixed some issues

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -5,18 +5,19 @@ import IPA from "../pages/ipa";
 import Dictionary from "../pages/browse";
 import WordDetailsPage from "../pages/word-details";
 import WordList from "../pages/browse/list-of-words/WordList";
+import Instructions from "../pages/instructions";
 
 const Router = () => {
 
     return (
         <Routes>
-            <Route path="/" element={<Home/>}></Route>
-            <Route path="/word" element={<WordDetailsPage/>}></Route>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/word" element={<WordDetailsPage/>}/>
             <Route path="/home" element={<Home/>}></Route>
             <Route path="/browse" element={<Dictionary/>}/>
             <Route path="/browse/list-of-words" element={<WordList/>}/>
-            <Route path="/ipa" element={<IPA/>}></Route>
-            <Route path="/browse" element={<Dictionary/>}></Route>
+            <Route path="/ipa" element={<IPA/>}/>
+            <Route path="/instructions" element={<Instructions/>}/>
         </Routes>
 
     );
