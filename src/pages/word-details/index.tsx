ec30c949ab400b9df030715d538263e9f190cf6e
bipsec <bip.sec22@gmail.com>
2023-09-07 00:40:16 +0600

    feat: word_details page modified

--- src/pages/word-details/index.tsx
+++ src/pages/word-details/index.tsx
@@ -1,8 +1,9 @@
-import * as React from "react";
-import {useEffect, useState} from "react";
-import {Box, Paper, Typography} from "@mui/material";
-import {useSearchParams} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Box, Paper, Typography } from "@mui/material";
+import { useSearchParams } from "react-router-dom";
 import axios from "axios";
+
+const API_URL = "http://0.0.0.0:8000/dictionary/word";
 
 
 const WordDetailsPage = () => {
@@ -10,17 +11,20 @@ const WordDetailsPage = () => {
     const word = searchParams.get("word");
     const [wordDetails, setWordDetails] = useState<any>({});
     const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
 
     useEffect(() => {
         if (word) {
-            axios.get(`http://localhost:8000/dictionary/word?word=${word}`)
+            axios
+                .get(`${API_URL}?word=${word}`)
                 .then((response) => {
                     setWordDetails(response.data);
-                    setLoading(false)
+                    setLoading(false);
                 })
                 .catch((error) => {
                     console.error("Error fetching data:", error);
                     setLoading(false);
+                    setError("An error occurred while fetching data.");
                 });
         }
     }, [word]);
@@ -28,25 +32,35 @@ const WordDetailsPage = () => {
     if (loading) {
         return <Typography>Loading...</Typography>;
     }
+
+    if (error) {
+        return <Typography>{error}</Typography>;
+    }
+
     return (
-        <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'}>
-            <Typography variant="h2" style={{textAlign: 'center'}}> Word details</Typography>
-            {wordDetails && <Paper sx={{p: 2, width: '600px', m: 2, boxShadow: 'none'}}>
-
-                <h4>{wordDetails?.word}:</h4>
-                {wordDetails.hasOwnProperty('similar_spellings') &&
-                    wordDetails?.similar_spellings.map((spelling: any) => {
-                        console.log("spelling", spelling)
-                        return (<Box key={spelling.id} sx={{mb: 1}}>
-                            <Typography variant="subtitle1">
-                                {spelling.meaning_no}: {spelling?.meaning}
-                            </Typography>
-                        </Box>)
-                    })}
-            </Paper>}
+        <Box
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+        >
+            <Typography variant="h2" align="center">
+                Word details
+            </Typography>
+            {wordDetails && (
+                <Paper sx={{ p: 2, width: "600px", m: 2, boxShadow: "none" }}>
+                    <Typography variant="h4">{wordDetails?.word}:</Typography>
+                    {wordDetails.hasOwnProperty("similar_spellings") &&
+                        wordDetails?.similar_spellings.map((spelling: any) => (
+                            <Box key={spelling.id} sx={{ mb: 1 }}>
+                                <Typography variant="subtitle1">
+                                    {spelling.meaning_no}: {spelling?.meaning}
+                                </Typography>
+                            </Box>
+                        ))}
+                </Paper>
+            )}
         </Box>
-
-
     );
 };
 
