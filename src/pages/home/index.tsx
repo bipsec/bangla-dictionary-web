dd76340630f47bf53d60052ac4935809e07b1575
bipsec <bip.sec22@gmail.com>
2023-09-11 00:11:41 +0600

    feat: contents updated

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -22,10 +22,23 @@ const Home = () => {
 
             <Paper style={{ padding: "16px", marginTop: "20px" }}>
                 {/* Random description content */}
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus.
-                Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida magna mi a libero. Fusce vulputate eleifend sapien. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nullam accumsan lorem in dui. Cras ultricies mi eu turpis hendrerit fringilla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In ac dui quis mi consectetuer lacinia. Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget,
+                <p>Welcome to the Online Bangla Dictionary - your comprehensive resource for all your Bengali language needs. Whether you're a native speaker looking for the meaning of a word or a language enthusiast eager to explore the rich linguistic heritage of Bangladesh and West Bengal, our web version is here to assist you.</p>
 
+                <p>Our Online Bangla Dictionary is designed to provide quick and accurate translations, definitions, and explanations for a wide range of words and phrases in both Bengali and English. With a user-friendly interface and a vast database of words, you can easily navigate through the world of Bengali language and culture.</p>
 
+                <p>Key features of our web version include:</p>
+
+                <ol>
+                    <li><strong>Bilingual Search:</strong> Easily search for words or phrases in both Bengali and English, ensuring you find the information you need, no matter which language you're comfortable with.</li>
+                    <li><strong>Detailed Definitions:</strong> Get in-depth definitions, synonyms, antonyms, and usage examples to understand the context and nuances of each word.</li>
+                    {/*<li><strong>Audio Pronunciations:</strong> Hear the correct pronunciation of words in Bengali, helping you improve your language skills.</li>*/}
+                    <li><strong>Word of the Day:</strong> Learn a new Bengali word every day and expand your vocabulary.</li>
+                    <li><strong>Language Learning Resources:</strong> Explore grammar guides, language tips, and cultural insights to enhance your understanding of Bengali.</li>
+                    <li><strong>Mobile-Friendly:</strong> Access our dictionary on your smartphone or tablet, making it convenient to use on the go.</li>
+                    <li><strong>Bookmark and Save:</strong> Save your favorite words for easy access in the future.</li>
+                </ol>
+
+                <p>Whether you're a student, traveler, or simply curious about the Bengali language, our Online Bangla Dictionary is your go-to resource. Start exploring the beauty of Bengali language and culture today!</p>
             </Paper>
         </div>
     );
